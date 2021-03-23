9a97452acc918f21ac599fa83a3bcf07fcc124d9
meldanor <bhaals@gmx.de>
2021-03-23 18:25:14 +0100

    Add third and fourth stream, color the score and print out month instead of number with week

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="mb-10 m-2 shadow-lg border-gray-800 bg-gray-100 rounded-md">
-    <div class="h-14 grid grid-cols-8 gap-1">
+    <div class="pt-2 h-24 grid grid-cols-8 gap-1">
       <div class="text-lg font-bold text-center col-span-6">{{game.name}}</div>
       <div class="text-lg col-span-2 flex flex-col pt-1">
-        <div class="self-center flex justify-center items-center h-12 w-12 ring-4 rounded-full ring-green-300 bg-green-500 font-bold">{{game.score}}</div>
+        <div class="self-center flex justify-center items-center h-12 w-12 ring-4 rounded-full font-bold" :class="scoreColor">{{game.score}}</div>
       </div>
     </div>
     <div class="text-xs h-12 flex justify-between p-1 font-semibold text-gray-800 bg-gray-300">
@@ -25,6 +25,21 @@
 </template>
 
 <script>
+const monthToStrings = {
+  1: "Januar",
+  2: "Februar",
+  3: "März",
+  4: "April",
+  5: "Mai",
+  6: "Juni",
+  7: "Juli",
+  8: "August",
+  9: "September",
+  10: "Oktober",
+  11: "November",
+  12: "Dezember",
+}
+
 export default {
   name: 'Game',
   data () {
@@ -40,7 +55,32 @@ export default {
   props: ["game"],
   computed: {
     releaseDate() {
-      return `W${this.game.releaseDate.week} M${this.game.releaseDate.month} ${this.game.releaseDate.year}`; 
+      return `${monthToStrings[this.game.releaseDate.month]} ${this.game.releaseDate.year}`;
+    },
+    scoreColor() {
+      if (this.game.score < 50) {
+        return  [
+          'ring-red-300',
+          'bg-red-500'
+        ];
+      }
+      else if (this.game.score < 75) {
+        return  [
+          'ring-yellow-300',
+          'bg-yellow-600'
+        ];
+      }
+      else if (this.game.score < 90) {
+        return  [
+          'ring-green-200',
+          'bg-green-300'
+        ];
+      }
+      return  [
+        'ring-green-300',
+        'bg-green-500',
+        'text-yellow-300'
+      ];
     }
   }
 }
